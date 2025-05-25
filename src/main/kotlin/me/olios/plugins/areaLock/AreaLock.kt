2155bda2df89cc831ed27090bebdff9fcaf0086c
Ariel <127093985+ariel3007@users.noreply.github.com>
2025-05-26 00:34:27 +0300

    Initial commit

---
+++ src/main/kotlin/me/olios/plugins/areaLock/AreaLock.kt
@@ -0,0 +1,14 @@
+package me.olios.plugins.areaLock
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class AreaLock : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
